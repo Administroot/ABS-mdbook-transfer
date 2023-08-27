975d35ed55e88c3e61f6b711747bfe79430e2103
administroot <1474668090@qq.com>
2023-08-27 19:13:39 +0800

    Add summary & transform suites

--- test.py
+++ test.py
@@ -3,12 +3,21 @@ import abs_mdbook_transfer
 
 
 class Test_transfer(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        self.summary = abs_mdbook_transfer.mdfile(abs_mdbook_transfer.MDPATH + "SUMMARY.md")
+        super().__init__(methodName)
+
     def test_format_print(self):
         self.assertLogs(abs_mdbook_transfer.format_print("ERROR", "Error msg"))
         self.assertLogs(abs_mdbook_transfer.format_print('WARN', "Warning msg"))
         self.assertLogs(abs_mdbook_transfer.format_print('INFO', "Info msg"))
 
+    def test_summary(self):
+        self.assertLogs(abs_mdbook_transfer.update_summary(self.summary))
+
+    def test_transform(self):
+        self.assertLogs(abs_mdbook_transfer.transform(abs_mdbook_transfer.MDPATH))
+
 
 if __name__ == '__main__':
     unittest.main()
-    
